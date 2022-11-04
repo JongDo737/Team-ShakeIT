d172bd2440c5097c1ded1c2a8fec0c9548146056
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-04 21:21:58 +0900

    update calendar entity

--- shake/src/main/java/com/example/shake/entity/Calendar.java
+++ shake/src/main/java/com/example/shake/entity/Calendar.java
@@ -15,13 +15,17 @@ public class Calendar {
     private Long id;
 
     @Column
-    private int type;
-
-    @Column(columnDefinition = "VARCHAR(45)")
-    private String session;
+    private int code;
+    // 1 : 국회의원 세미나
+    // 2 : 본회의 일정
+    // 3 : 위원회 별 일정
+    // 4 : 국회의장 일정
+    // 5 : 공청회 일정
 
     @Column(columnDefinition = "VARCHAR(45)")
     private String title;
+    @Column(columnDefinition = "VARCHAR(45)")
+    private String committee_name;
 
     @Column(columnDefinition = "VARCHAR(45)")
     private String date;
@@ -29,6 +33,7 @@ public class Calendar {
     @Column(columnDefinition = "VARCHAR(45)")
     private String time;
 
-    @Column(columnDefinition = "VARCHAR(45)")
-    private String daesu;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String url;
+
 }
