4ff6b74db60fc014e2128a2bc90791a46f181a7f
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-04 20:24:20 +0900

    complete making Dtos

--- shake/src/main/java/com/example/shake/dto/LegislativeStatusDto.java
+++ shake/src/main/java/com/example/shake/dto/LegislativeStatusDto.java
@@ -1,4 +1,24 @@
 package com.example.shake.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LegislativeStatusDto {
+    int id;
+    String bill_no;
+    String bill_name;
+    String age;
+    String pro_kind;
+    String curr_committee;
+    String noti_end_dt;
+    String link;
+    String proposer;
+    String bill_id;
+    String committee_id;
 }
--- shake/src/main/java/com/example/shake/dto/ProcessedPetitionDto.java
+++ shake/src/main/java/com/example/shake/dto/ProcessedPetitionDto.java
@@ -1,4 +1,24 @@
 package com.example.shake.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ProcessedPetitionDto {
+    int id;
+    String num;
+    String age;
+    String name;
+    String proposer;
+    String approver;
+    String pro_dt;
+    String curr_committee;
+    String url;
+    String bill_id;
+    String committee_id;
 }
