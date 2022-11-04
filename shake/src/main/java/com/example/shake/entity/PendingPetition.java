cad07bc432766f2a6bd023bb76eabb7f67b15756
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-04 20:41:38 +0900

    PendingPetition entity

---
+++ shake/src/main/java/com/example/shake/entity/PendingPetition.java
@@ -0,0 +1,36 @@
+package com.example.shake.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "pending_petition")
+public class PendingPetition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String num;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String age;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String name;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String proposer;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String approver;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String pro_dt;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String curr_committee;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String url;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String bill_id;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String committee_id;
+
+}
