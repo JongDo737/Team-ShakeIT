f74c14473711c17d6dd416d2b05c05b4394e010f
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-04 20:27:47 +0900

    bill entity

---
+++ shake/src/main/java/com/example/shake/entity/Bill.java
@@ -0,0 +1,40 @@
+package com.example.shake.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+@Data
+@Entity
+@Table(name = "Bill")
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String daesu;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String bill_num;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String bill_name;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String propersor;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String committee_nm;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String proc_result;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String proc_date;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String curr_trans_dt;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String announce_dt;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String url;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String rgs_proc_dt;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String bill_id;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String curr_committee_id;
+
+}
