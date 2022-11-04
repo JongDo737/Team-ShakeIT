1b9cb5a2e5cb29a0a24dc194c4e3e08a4efba289
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-04 20:19:29 +0900

    BillDto

--- shake/src/main/java/com/example/shake/dto/BillDto.java
+++ shake/src/main/java/com/example/shake/dto/BillDto.java
@@ -1,4 +1,27 @@
 package com.example.shake.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BillDto {
+    int id;
+    String daesu;
+    String bill_num;
+    String bill_name;
+    String propersor;
+    String committee_nm;
+    String proc_result;
+    String proc_date;
+    String curr_trans_dt;
+    String announce_dt;
+    String url;
+    String rgs_proc_dt;
+    String bill_id;
+    String curr_committee_id;
 }
