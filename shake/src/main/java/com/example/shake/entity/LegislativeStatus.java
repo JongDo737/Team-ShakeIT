b882d6b088ddcc4c6232c6594c9a1088469c166f
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-04 20:40:19 +0900

    LegislativeStatus entity

---
+++ shake/src/main/java/com/example/shake/entity/LegislativeStatus.java
@@ -0,0 +1,35 @@
+package com.example.shake.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "legislativeStatus")
+public class LegislativeStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String bill_no;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String bill_name;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String age;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String pro_kind;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String curr_committee;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String noti_end_dt;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String link;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String proposer;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    String bill_id;
+    @Column(columnDefinition = "VARCHAR(45)")
+    String committee_id;
+
+}
